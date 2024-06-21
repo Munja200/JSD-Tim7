b0df795602bca9a5d284d1c888d641172329b524
Radiša Stojkić <92645987+Munja200@users.noreply.github.com>
2024-06-21 19:29:22 +0200

    Update generator.py

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -6,8 +6,7 @@ from utils import module_path
 def generate_recipe(recipe_model):
     env = Environment(loader=FileSystemLoader(module_path('templates')), autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('recipeTemplate.jinja')
-    html_output = template.render(model=recipe_model, slika1=module_path('icons\stopwatch.png'), slika2=module_path('icons\calories-calculator.png'))
-    print(module_path('icons\stopwatch.png'))
+    html_output = template.render(model=recipe_model)
     current_directory = os.getcwd()
     output_file_name = 'output.html'
     path = os.path.join(current_directory,output_file_name)
