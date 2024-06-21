15e509ab1ec4160955db7fa9e2a083523de04415
Mihailo Veljić <veljic.ra31.2019@unus.ac.rs>
2024-06-21 17:00:52 +0200

    Changed in pyproject.toml file

--- src/main.py
+++ src/main.py
@@ -3,21 +3,16 @@ from metamodel import recipe
 import logging
 import sys
 
-import os
-
 
 def main():
     try:
         print(sys.argv[1:])
         args = sys.argv[1:]
-        current_directory = os.getcwd()
         print(current_directory)
         if len(args) == 0:
             generator.generate_recipe(recipe.init_recipe_metamodel())
         elif len(args) == 1:
             generator.generate_recipe(recipe.init_recipe_metamodel(args[0]))
-        elif len(args) == 2:
-            generator.generate_recipe(recipe.init_recipe_metamodel(args[0]), args[1])
         else:
             logging.exception("Losi argumenti")
             
